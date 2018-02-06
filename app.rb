14f9f0da8cad369bf50faef810a811474e4d76d2
carlostrapet <trapet_llamas@hotmail.com>
2018-02-06 12:21:31 +0000

    added shvarg

--- app.rb
+++ app.rb
@@ -4,26 +4,17 @@ get '/' do
   "Hello!"
 end
 
-get '/secret' do
-  "Maker's is cool"
+# get '/random-cat' do
+#   @name = ["Amigo", "Oscar", "Viking"].sample
+#   erb(:index)
+# end
+
+get '/cat-form' do 
+  erb(:cat_form)
 end
 
-get '/carlos' do
-  "Oi! Carlos!"
-end
-
-get '/heather' do
-  "Hiya!"
-end
-
-get '/random-cat' do
-  @name = ["Amigo", "Oscar", "Viking"].sample
-  erb(:index)
-end
-
-get '/named-cat' do
+post '/named-cat' do
   p params
   @name = params[:name]
   erb(:index)
 end
-
